15abcc2202f4faa309c2334b2c8dfb715a9f77d8
LaomoBK <laomobk@163.com>
2020-05-31 15:41:55 +0800

    setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='nhttp',
-    version='1.0',
+    version='1.1build',
     description='A portable http server package',
     author='LaomoBK',
     packages=['nhttp', 'nhttp.server']
